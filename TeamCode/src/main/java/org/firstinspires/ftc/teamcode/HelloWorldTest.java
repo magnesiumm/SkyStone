98cb77057fde94bd93f89920b98bf49f78442fc5
magnesiumm <null>
2019-10-24 18:02:54 -0400

    First Test Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+//@Author AJ Radik
+
+public class HelloWorldTest {
+
+    public static void main(String[] args) {
+        System.out.println("Helow, Wurld!");
+    }
+
+}
