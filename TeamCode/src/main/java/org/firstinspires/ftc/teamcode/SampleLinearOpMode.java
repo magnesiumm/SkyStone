3eedd6bf1c54c4e0e44e37c1eaa05402797faf61
michaelg29 <michaelgriecocoding@gmail.com>
2019-11-13 13:17:47 -0500

    Added sample op modes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleLinearOpMode.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="SampleLinearOpMode", group="Linear Opmode")
+@Disabled
+public class SampleLinearOpMode extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private DcMotor leftMotor = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        leftMotor = hardwareMap.dcMotor.get("left_drive");
+        leftMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+
+            idle();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleOpMode.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="SampleOpMode", group="Iterative Opmode")
+@Disabled
+public class SampleOpMode extends OpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+
+    // private DcMotor leftMotor = null;
+    // private DcMotor rightMotor = null;
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+    }
+
+    @Override
+    public void init_loop() {}
+
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Status", "Running: " + runtime.toString());
+    }
+
+    @Override
+    public void stop() {}
+}
