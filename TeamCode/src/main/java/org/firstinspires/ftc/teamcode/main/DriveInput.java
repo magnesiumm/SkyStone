f008af7890d3bfde7b6615dafc1035d41756c423
brianl20 <brianl20@bronxvilleschool.org>
2020-01-15 15:33:17 -0500

    KARL MARX DID SOME THINGS AND WROTE A BOOK.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/DriveInput.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/DriveInput.java
@@ -41,108 +41,49 @@ public class DriveInput extends MainProcess {
      */
     @Override
     public State main() {
-        // reset
-        //setMotors(0, Direction.FORWARD);
-        Direction dir = null;
-
-        // get stick values
-        float y = -1 * opMode.gamepad1.left_stick_y;
         float x = opMode.gamepad1.left_stick_x;
+        float y = opMode.gamepad1.left_stick_y;
+        float drive_pow = -1 * y * maxPow;
+        float turn_pow = -1 * x * maxTurningPow;
 
-        // drive
-        float drive_pow = Math.abs(y * maxPow);
+        float left_pow, right_pow;
 
-        // TODO: subtract or add for turning
+        left_pow = drive_pow + turn_pow;
+        right_pow = drive_pow + turn_pow;
 
-        dir = (y >= 0) ? Direction.FORWARD : Direction.REVERSE;
-        setMotors(drive_pow, dir);
+        setLeftMotor(left_pow);
+        setRightMotor(right_pow);
 
-        // turn
-        float turn_pow = turn(x, drive_pow);
-
-        return new DriveState(drive_pow, 0, dir, x, y);
+        return new DriveState(drive_pow, turn_pow, x, y);
     }
 
     /*
-    set power to both motors
+    set left motor power/direction
      */
-    private void setMotors(float power, Direction dir) {
-        leftDrive.setPower(power);
-        leftDrive.setDirection(dir);
-        //leftDrive.setDirection(Direction.REVERSE);
-
-        rightDrive.setPower(power);
-        // TODO: Mechanics reverse motors
-        rightDrive.setDirection((dir == Direction.FORWARD) ? Direction.REVERSE : Direction.FORWARD);
+    public void setLeftMotor(float pow) {
+        leftDrive.setDirection(pow >= 0 ? Direction.FORWARD : Direction.REVERSE);
+        // Direction.FORWARD if pow >= 0 else Direction.REVERSE
+        leftDrive.setPower(Math.abs(pow));
     }
 
     /*
-    turn the robot by decreasing power in one wheel and increasing in other
+    set right motor power/direction
      */
-    private float turn(float x, double drive_pow)
-    {
-        if (Math.abs(x) < .05) {
-            return 0;
-        }
-
-        // get current demanded power
-        float pow = Math.abs(x * maxTurningPow);
-
-        // get current power on motors
-        double leftPow = drive_pow;
-        double rightPow = drive_pow;
-
-        if (leftDrive.getDirection() == Direction.REVERSE) {
-            leftPow *= -1;
-        }
-        if (rightDrive.getDirection() == Direction.REVERSE) {
-            rightPow *= -1;
-        }
-
-        if (x < 0) {
-            // turn left
-
-            leftPow -= pow;
-            rightPow += pow;
-        } else if (x > 0) {
-            // turn right
-
-            leftPow += pow;
-            rightPow -= pow;
-        }
-
-        leftDrive.setDirection(leftPow >= 0 ? Direction.FORWARD : Direction.REVERSE);
-        rightDrive.setDirection(rightPow >= 0 ? Direction.FORWARD : Direction.REVERSE);
-
-        leftDrive.setPower(Math.abs(leftPow));
-        rightDrive.setPower(Math.abs(rightPow));
-
-        return pow;
-    }
-
-    /*
-    reverse direction of specified motor
-     */
-    private void reverseDirection(int motor) {
-        if (motor == LEFT_MOTOR) {
-            leftDrive.setDirection((leftDrive.getDirection() == Direction.FORWARD) ? Direction.REVERSE : Direction.FORWARD);
-        } else if (motor == RIGHT_MOTOR) {
-            rightDrive.setDirection((rightDrive.getDirection() == Direction.FORWARD) ? Direction.REVERSE : Direction.FORWARD);
-        }
+    public void setRightMotor(float pow) {
+        rightDrive.setDirection(pow >= 0 ? Direction.FORWARD : Direction.REVERSE);
+        rightDrive.setPower(Math.abs(pow));
     }
 }
 
 class DriveState extends State {
     public float drivePower;
     public float turningPower;
-    public Direction direction;
     public float stickX;
     public float stickY;
 
-    public DriveState(float drivePower, float turningPower, Direction direction, float stickX, float stickY) {
+    public DriveState(float drivePower, float turningPower, float stickX, float stickY) {
         this.drivePower = drivePower;
         this.turningPower = turningPower;
-        this.direction = direction;
         this.stickX = stickX;
         this.stickY = stickY;
     }
